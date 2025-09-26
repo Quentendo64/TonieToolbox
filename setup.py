24105ea299f7ef72804f4a60863a80fbbf601893
Quentin Wohlfeil <20115130+Quentendo64@users.noreply.github.com>
2025-09-26 10:32:09 +0200

    Change license from MIT to GPLv3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Topic :: Multimedia :: Sound/Audio :: Conversion",
     ],
